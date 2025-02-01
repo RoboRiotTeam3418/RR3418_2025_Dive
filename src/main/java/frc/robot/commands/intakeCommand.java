5fbef9839e5e1a02ff8d3e936d1aa367ff7d1230
AL Snodgrass <roboriotdev1@gmail.com>
2025-02-01 13:11:48 -0600

    re add intake files

---
+++ src/main/java/frc/robot/commands/intakeCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralIntake;
+
+public class intakeCommand extends Command {
+    double value;
+    CoralIntake intake;
+    boolean active;
+    public intakeCommand(double intakeValue, CoralIntake subsystem, boolean Active){
+        this.value=intakeValue;
+        this.intake=subsystem;
+        this.active=Active;
+        addRequirements(subsystem);
+    }
+    @Override
+    public void execute(){
+        if (active) {
+        intake.Intake(value);    
+        } else {
+        intake.Intake(.1);
+        }
+    }
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            intake.Intake(0);
+        }
+    }
+}

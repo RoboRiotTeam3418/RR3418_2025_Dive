1ff0eb68888e67e28ba0b0acc53e8b5af8c4c13e
AL Snodgrass <roboriotdev1@gmail.com>
2025-02-17 18:05:33 -0600

    updates to deadband util

--- src/main/java/frc/robot/util/math/Deadbands.java
+++ src/main/java/frc/robot/util/math/Deadbands.java
@@ -6,23 +6,20 @@ import frc.robot.Setup;
 
 public class Deadbands{
     public static Deadbands instance = new Deadbands();
-
-    public static Deadbands getInstance() {
-        if (instance == null) {
-            instance = new Deadbands();
-        }
-        return instance;
-    }   
     private Deadbands(){}
 
-    public boolean isWithin(double valueA, double valueB, double deadband){
+    public static boolean isWithin(double valueA, double valueB, double deadband){
         return (valueA < valueB+deadband)&&(valueA>valueB-deadband);
     }
 
-    public boolean isOutside(double valueA, double valueB, double deadband){
+    public static boolean isOutside(double valueA, double valueB, double deadband){
         return (valueA > valueB+deadband)&&(valueA<valueB-deadband);
     }
-    public boolean isGreater(double valueA, double deadband){
-        return valueA>deadband || valueA<-deadband;
+    public static boolean isGreater(double valueA, double deadband){
+        return Math.abs(valueA)>deadband;
+    }
+
+    public static boolean isLess(double valueA, double deadband){
+        return Math.abs(valueA)<deadband ;
     }
 }
